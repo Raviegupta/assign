5593329f5a5993f97c0f7d4671cb64ea488d2a65
Raviegupta <raviegupta153@gmail.com>
2022-12-04 19:25:55 +0530

    version 1.0

---
+++ script.js
@@ -0,0 +1,8 @@
+let one = document.querySelector('.one')
+let oneImage = document.querySelector('.file-js')
+
+one.addEventListener('click', () => {
+    oneImage.innerHTML = `<img src="images/download.jpg" alt="">`
+})  
+
+
